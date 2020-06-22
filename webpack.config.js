5cd556c445be75d9ea8207acd164a5db2f99cdcc
VaskoDeGama <vasr161095@gmail.com>
2020-06-22 11:56:18 +0800

    add webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,1 +1,12 @@
-gti
+const path = require('path')
+
+
+module.exports = {
+    context: path.resolve(__dirname, 'src'),
+    mode: 'development',
+    entry: './index.js',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist')
+    }
+}
