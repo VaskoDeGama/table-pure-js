c73dcfc1a431ef3e23b3b78de6925a03ee5f2c3a
VaskoDeGama <vasr161095@gmail.com>
2020-07-19 17:05:12 +0800

    env setup

--- src/pages/TcellPage.js
+++ src/pages/TcellPage.js
@@ -1,5 +1,5 @@
 import {Page} from '@core/Page'
-import {createStore} from '@core/createStore'
+import {createStore} from '@core/store/createStore'
 import {normalizeInitialState} from '@/store/initialState'
 import {rootReducer} from '@/store/rootReducer'
 import {debounce, storage} from '@core/Utils'
